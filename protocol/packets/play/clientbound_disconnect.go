e77ea081588cbab5a623ab0feb642c43f0c26993
Mohan <mohanavel15@protonmail.com>
2024-09-24 20:40:17 +0530

    disconnect packet

---
+++ protocol/packets/play/clientbound_disconnect.go
@@ -0,0 +1,32 @@
+package play
+
+import "amethyst/protocol"
+
+const ClientBoundDisconnectPacketID byte = 0x40
+
+type ClientBoundDisconnect struct {
+	Reason protocol.Chat
+}
+
+func (pk ClientBoundDisconnect) Marshal() protocol.Packet {
+	return protocol.MarshalPacket(
+		ClientBoundDisconnectPacketID,
+		pk.Reason,
+	)
+}
+
+func UnmarshalClientBoundDisconnect(packet protocol.Packet) (ClientBoundDisconnect, error) {
+	var pk ClientBoundDisconnect
+
+	if packet.ID != ClientBoundDisconnectPacketID {
+		return pk, protocol.ErrInvalidPacketID
+	}
+
+	if err := packet.Scan(
+		&pk.Reason,
+	); err != nil {
+		return pk, err
+	}
+
+	return pk, nil
+}
